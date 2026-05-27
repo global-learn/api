4d5bf06ec802984bdf20443e5f3d4445b9b23b6a
toooweee <byprob03@gmail.com>
2026-05-27 12:42:57 +0500

    fix: (eslint) unused errors

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,11 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '@/infra/prisma/prisma.service';
 
 @Injectable()
-export class UserService {
-  constructor(private readonly prismaService: PrismaService) {}
-
-  async findAll() {
-    console.log(await this.prismaService.user.findMany());
-  }
-}
+export class UserService {}
